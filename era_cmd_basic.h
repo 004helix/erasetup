3ff9197afdbcf0bb76f70980d00c4fb1ad51f125
Raman Shishniou <rommer@ibuffed.com>
2015-11-16 16:09:58 +0300

    cleanup

--- era_cmd_basic.h
+++ era_cmd_basic.h
@@ -5,8 +5,6 @@
 #ifndef __ERA_CMD_BASIC_H__
 #define __ERA_CMD_BASIC_H__
 
-#define RANDOM_DEVICE "/dev/urandom"
-
 #define MIN_CHUNK_SIZE 8   /* sectors: 4k */
 #define DEF_CHUNK_SIZE 128 /* sectors: 64k */
 
